d59bc4c669b11bb3c337051ae4ae4a25dc21539d
mcarolee <mcarolee@student.21-school.ru>
2021-05-21 09:18:54 +0500

    second commit

--- ex00/Program.cs
+++ ex00/Program.cs
@@ -5,7 +5,21 @@ namespace ex00
     class Program
     {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            double sum;
+            double rate;
+            int term;
+            int selectedMonth;
+            double payment;
+
+            if (args.Length < 5 || !double.TryParse(args[0], out sum) || sum < 0
+                || !double.TryParse(args[1], out rate) || rate < 0
+                || !int.TryParse(args[2], out term) || term < 1
+                || !int.TryParse(args[3], out selectedMonth) || selectedMonth < 0
+                || !double.TryParse(args[4], out payment) || payment < 0) {
+                Console.WriteLine("Ошибка ввода. Проверьте входные данные и повторите запрос.");
+                return;
+            }
+
         }
     }
 }
