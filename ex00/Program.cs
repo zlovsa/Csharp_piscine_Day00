c7e8b3fdc0a7a44ec51f34e8344b8450e5ba7124
mcarolee <mcarolee@student.21-school.ru>
2021-05-22 22:52:58 +0500

    done

--- ex00/Program.cs
+++ ex00/Program.cs
@@ -9,7 +9,7 @@ double payment;
 if (args.Length < 5 || !double.TryParse(args[0], out sum) || sum < 0
     || !double.TryParse(args[1], out rate) || rate < 0
     || !int.TryParse(args[2], out term) || term < 1
-    || !int.TryParse(args[3], out selectedMonth) || selectedMonth < 0
+    || !int.TryParse(args[3], out selectedMonth) || selectedMonth < 1
     || !double.TryParse(args[4], out payment) || payment < 0) {
     Console.WriteLine("Ошибка ввода. Проверьте входные данные и повторите запрос.");
     return;
@@ -28,40 +28,49 @@ double DaysInYear(int Year) {
     return dt1.Subtract(dt0).TotalDays;
 }
 
-double TotalPay(double sum, double rate, int term, int selectedMonth, double payment, bool decreasePayment) {
-    double sumPayments = 0;
+double OverPay(double sum, double rate, int term, int selectedMonth, double payment, bool decreasePayment, bool print) {
+    double sumPercents = 0;
     double monthlyPayment = MonthlyPayment(sum, rate, term);
     DateTime dt0 = DateTime.Now;
     dt0 = new DateTime(dt0.Year, dt0.Month, 1, 12, 0, 0);
     int n = 1;
 
+    if (print) {
+        Console.WriteLine(decreasePayment ? "График платежей с уменьшением платежа:" : "График платежей с уменьшением срока:");
+        Console.WriteLine("   Дата             Платеж                ОД          Проценты        Остаток долга");
+        double totalOverpay = OverPay(sum, rate, term, selectedMonth, 0, false, false);
+        Console.WriteLine($"{"",-11:d}{monthlyPayment,15:N2} р.{sum,15:N2} р.{totalOverpay,15:N2} р.{sum,18:N2} р.");
+    }
     while (sum > 0) {
         DateTime dt1 = dt0.AddMonths(1);
         double deltaDays = dt1.Subtract(dt0).TotalDays;
         double percents = sum * rate / 100 * deltaDays / DaysInYear(dt0.Year);
         double debtpayment = Math.Min(monthlyPayment - percents, sum);
         sum -= debtpayment;
-        sumPayments += debtpayment + ((sum > 0) ? percents : 0);
+        sumPercents += ((sum > 0) ? percents : 0);
         if (n == selectedMonth) {
             sum -= payment;
-            sumPayments += payment;
             if (decreasePayment)
                 monthlyPayment = MonthlyPayment(sum, rate, term - selectedMonth);
         }
+        if (sum > 0 && print)
+            Console.WriteLine($"{dt1,-11:d}{monthlyPayment,15:N2} р.{debtpayment,15:N2} р.{percents,15:N2} р.{sum,18:N2} р.");
         dt0 = dt1;
         n++;
     }
-    
-    return (sumPayments);
+    if (print)
+        Console.WriteLine();
+
+    return (sumPercents);
 }
 
-double decreasePaymentOverpay = TotalPay(sum, rate, term, selectedMonth, payment, true) - sum;
-double decreaseTermOverpay = TotalPay(sum, rate, term, selectedMonth, payment, false) - sum;
-Console.WriteLine("Переплата при уменьшении платежа: {0:N2}р.", decreasePaymentOverpay);
-Console.WriteLine("Переплата при уменьшении срока: {0:N2}р.", decreaseTermOverpay);
+double decreasePaymentOverpay = OverPay(sum, rate, term, selectedMonth, payment, true, true);
+double decreaseTermOverpay = OverPay(sum, rate, term, selectedMonth, payment, false, true);
+Console.WriteLine($"Переплата при уменьшении платежа: {decreasePaymentOverpay:N2} р.");
+Console.WriteLine($"Переплата при уменьшении срока: {decreaseTermOverpay:N2} р.");
 if (decreaseTermOverpay < decreasePaymentOverpay)
-    Console.WriteLine("Уменьшение срока выгоднее уменьшения платежа на {0:N2}р.", decreasePaymentOverpay - decreaseTermOverpay);
+    Console.WriteLine($"Уменьшение срока выгоднее уменьшения платежа на {decreasePaymentOverpay - decreaseTermOverpay:N2} р.");
 else if (decreaseTermOverpay > decreasePaymentOverpay)
-    Console.WriteLine("Уменьшение платежа выгоднее уменьшения срока на {0:N2}р.", decreaseTermOverpay - decreasePaymentOverpay);
+    Console.WriteLine($"Уменьшение платежа выгоднее уменьшения срока на {decreaseTermOverpay - decreasePaymentOverpay:N2} р.");
 else
     Console.WriteLine("Переплата одинакова в обоих вариантах.");
--- ex01/Program.cs
+++ ex01/Program.cs
@@ -1,11 +1,62 @@
 ﻿using System;
 
-namespace ex01
-{
-    class Program
-    {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
-        }
-    }
+string[] namevoc = System.IO.File.ReadAllLines(@"..\..\..\us.txt");
+
+Console.WriteLine(">Enter name:");
+string name = Console.ReadLine();
+if (name == "") {
+    Console.WriteLine("Your name was not found.");
+    return;
 }
+
+bool ValidChar(char c) {
+    return (char.IsLetter(c) || char.IsWhiteSpace(c) || c == '-');
+}
+
+foreach (char c in name)
+    if (!ValidChar(c)) {
+        Console.WriteLine("Name may contain letters, whitespaces and hyphens.");
+        return;
+    }
+
+foreach (string vname in namevoc)
+    if (name == vname) {
+        Console.WriteLine($"Hello, {name}!");
+        return;
+    }
+
+int Levenstein(string a, string b, int curr) {
+    if (curr > 3)
+        return curr;
+    if (a.Length == 0)
+        return b.Length;
+    if (b.Length == 0)
+        return a.Length;
+    if (a[0] == b[0])
+        return Levenstein(a.Substring(1), b.Substring(1), curr);
+    return 1 + Math.Min(Levenstein(a.Substring(1), b, curr + 1),
+         Math.Min(Levenstein(a, b.Substring(1), curr + 1),
+         Levenstein(a.Substring(1), b.Substring(1), curr + 1)));
+}
+
+int[] lev = new int[namevoc.Length];
+int l = 1;
+while (l <= 3) {
+    int i = 0;
+    while (i < namevoc.Length) {
+        if (lev[i] == 0)
+            lev[i] = Levenstein(name, namevoc[i], 0);
+        if (lev[i] == l) {
+            Console.WriteLine($">Did you mean “{namevoc[i]}”? Y/N");
+            string answer = Console.ReadLine();
+            if (answer == "Y") {
+                Console.WriteLine($"Hello, {namevoc[i]}!");
+                return;
+            }
+        }
+        i++;
+    }
+    l++;
+}
+
+Console.WriteLine("Your name was not found.");
